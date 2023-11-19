011f5f96ba51d027bb40959de3bdfa9f91778e17
AdriDevelopsThings <git@adridoesthings.com>
2023-11-19 17:25:39 +0100

    modify example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,17 @@
+use std::{fs, env::args, process::exit};
+
 use risc_v_emulator::{Cpu, Bus};
 
 fn main() {
-    let mut cpu = Cpu::new(Bus::new(1024 * 1024, vec![0, 0, 0, 0]));
+    let args = args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        eprintln!("Run <command> <path to binary>");
+        exit(1);
+    }
+    let path = args.get(1).unwrap();
+    let code = fs::read(path)
+        .expect("Error while reading binary file");
+    let mut cpu = Cpu::new(Bus::new(1024 * 1024, code));
     loop {
         let ret = cpu.run_next_instruction();
         if ret.is_err() {

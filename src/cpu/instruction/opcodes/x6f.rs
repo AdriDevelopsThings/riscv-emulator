10a8a8b3e5d8470b636c88a252a625e2887ea606
AdriDevelopsThings <git@adridoesthings.com>
2023-11-22 17:17:42 +0100

    remove wrong comment

--- src/cpu/instruction/opcodes/x6f.rs
+++ src/cpu/instruction/opcodes/x6f.rs
@@ -14,7 +14,6 @@ use crate::{
 /// Run an instruction with opcode=1101111
 /// JAL jump and link
 pub fn run_x6f_instruction(cpu: &mut Cpu, instruction: Instruction) -> Result<(), RiscVException> {
-    // instruction is a JType but ins basicly an UType
     let instruction = JTypeInstruction::parse_instruction(instruction);
     let imm = i20_to_u64(instruction.get_full_immediate()).wrapping_mul(2);
     let new_pc = cpu.read_register(PC_REGISTER_INDEX)?.wrapping_add(imm);

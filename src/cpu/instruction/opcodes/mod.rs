cdb87f0cf36a88fd54559e349b127c1524c5beb9
AdriDevelopsThings <git@adridoesthings.com>
2023-11-22 17:31:19 +0100

    add instruction x67 (JALR)

--- src/cpu/instruction/opcodes/mod.rs
+++ src/cpu/instruction/opcodes/mod.rs
@@ -2,7 +2,7 @@ use crate::{cpu::Cpu, exception::RiscVException};
 
 use self::{
     x13::run_x13_instruction, x17::run_x17_instruction, x33::run_x33_instruction,
-    x37::run_x37_instruction, x6f::run_x6f_instruction,
+    x37::run_x37_instruction, x67::run_x67_instruction, x6f::run_x6f_instruction,
 };
 
 use super::{instruction_type::parse_opcode, Instruction};
@@ -11,6 +11,7 @@ mod x13;
 mod x17;
 mod x33;
 mod x37;
+mod x67;
 mod x6f;
 
 impl Cpu {
@@ -21,6 +22,7 @@ impl Cpu {
             0x17 => run_x17_instruction(self, instruction),
             0x33 => run_x33_instruction(self, instruction),
             0x37 => run_x37_instruction(self, instruction),
+            0x67 => run_x67_instruction(self, instruction),
             0x6f => run_x6f_instruction(self, instruction),
             _ => Err(RiscVException::IllegalInstruction),
         }
---
+++ src/cpu/instruction/opcodes/x67.rs
@@ -0,0 +1,23 @@
+use crate::{
+    cpu::{
+        instruction::{
+            instruction_type::{ITypeInstruction, TypeInstruction},
+            Instruction,
+        },
+        registers::PC_REGISTER_INDEX,
+    },
+    exception::RiscVException,
+    utils::i12_to_u64,
+    Cpu,
+};
+
+/// Run an instruction with opcode=1100111
+/// JALR jump and link register
+pub fn run_x67_instruction(cpu: &mut Cpu, instruction: Instruction) -> Result<(), RiscVException> {
+    let instruction = ITypeInstruction::parse_instruction(instruction);
+    let imm = i12_to_u64(instruction.imm);
+    let new_pc = cpu.read_register(instruction.rs1)?.wrapping_add(imm) & 0xFFFFFFFFFFFFFFFE;
+    cpu.pc_increment = false;
+    cpu.write_register(PC_REGISTER_INDEX, new_pc)?;
+    cpu.write_register(instruction.rd, new_pc + 4)
+}
